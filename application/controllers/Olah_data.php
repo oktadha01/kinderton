e012c0bc266275fef6269546b3f9dd66a056aaa8
oktadha01 <oktadha01@gmail.com>
2022-11-03 20:03:23 +0700

    config, olah data control

--- application/controllers/Olah_data.php
+++ application/controllers/Olah_data.php
@@ -8,9 +8,6 @@ class Olah_data extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->model('m_olah_data');
-		$this->load->library('form_validation');
-		$this->load->helper('url');
-		$this->load->helper('form');
 	}
 
 	public function index()
