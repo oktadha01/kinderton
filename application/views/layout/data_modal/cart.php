815ed3f4477798e6a2c9ff152d5bf3e4c4bca7a7
oktadha01 <oktadha01@gmail.com>
2022-11-10 10:12:49 +0700

    wa cart

--- application/views/layout/data_modal/cart.php
+++ application/views/layout/data_modal/cart.php
@@ -1399,10 +1399,10 @@ if ($query->num_rows() > 0) {
                         $('.select2-selection,.pilih-bukti-transfer').removeClass('btn-disabled').removeAttr('disabled', true);
                         $('#an-pengirim,#nominal,#bank-pengirim,#nm-bukti-transfer').removeClass('input_disabled').removeAttr('disabled', true);
                         var kode_pesanan = $('#kode-pesanan').val();
-                        var nohp = '(<?php echo $useradmin->kontak; ?>)';
+                        var nohp = <?php echo $useradmin->kontak; ?>;
                         // var pesan = 'Terikasih sudah order, pesanan kamu segara kami kiri ke alamat anda ...';
-                        var pesan = 'Hallo kak saya mau konfirmasi pembayaran dengan kode pesanans ' + kode_pesanan;
-                        var linkWA = 'https://api.whatsapp.com/send?phone=' + nohp + '&text=' + pesan;
+                        var pesan = 'Hallo kak saya mau konfirmasi pembayaran dengan kode pesanan (' + kode_pesanan + ')';
+                        var linkWA = 'https://api.whatsapp.com/send?phone='+nohp+'&text='+pesan;
                         window.location = linkWA;
                     },
                     error: function() {
